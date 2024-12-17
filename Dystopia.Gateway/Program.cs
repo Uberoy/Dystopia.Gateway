0ed5358169bc9b88deb02377ecaa1d339e8d5a8c
Uberoy <143390218+Uberoy@users.noreply.github.com>
2024-12-17 11:55:29 +0100

    Removed swagger for ocelot

--- Dystopia.Gateway/Program.cs
+++ Dystopia.Gateway/Program.cs
@@ -6,15 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
-
 var app = builder.Build();
 
 app.UseOcelot().Wait();
 
-app.UseSwaggerForOcelotUI(opt =>
-{
-    opt.PathToSwaggerGenerator = "/swagger/docs";
-});
-
-app.Run();
+app.Run("http://0.0.0.0:5000");
