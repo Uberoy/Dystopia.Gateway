4641259ad620a7589e19ed78dd991d85ad975485
Uberoy <joacim.jansson@iths.se>
2024-12-18 22:12:06 +0100

    Updated to use CORS

--- Dystopia.Gateway/Program.cs
+++ Dystopia.Gateway/Program.cs
@@ -3,10 +3,23 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Support", policy =>
+    {
+        policy.WithOrigins("http://localhost:7777")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+
+app.UseCors("Support");
 
 app.UseOcelot().Wait();
 
